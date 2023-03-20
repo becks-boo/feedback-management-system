a2f90442e19cee345f7b100a4583543a816fc2e9
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-03-20 13:31:14 +0100

    Pushed initialized project

---
+++ src/main/java/com/stein/ausbilderportal/AusbilderPortalApplication.java
@@ -0,0 +1,13 @@
+package com.stein.ausbilderportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusbilderPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AusbilderPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stein/ausbilderportal/AusbilderPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stein.ausbilderportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusbilderPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
